58a80e2874d4f16ada23293da6241833488e06ed
Prabhat Rana <153556000+prabhatrana666@users.noreply.github.com>
2026-08-20 20:16:00 +0530

    footer section wip

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import How from './components/how_it_works/How.jsx';
 
 createRoot(document.getElementById('root')).render(
